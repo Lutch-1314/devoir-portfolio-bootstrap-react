e8c5a3fe3259451aa2c7bd768732b1dcb15be51d
Lucie Aimar <lucie.aimar13@gmail.com>
2025-05-09 22:57:18 +0200

    most of html homepage completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,84 @@ import React from "react";
 class App extends React.Component {
   render() {
     return(
-      <div className="App">
-        <header className="App-header">
+      <body className="">
+        <nav>
+          <div>John Doe</div>
+          <a href="/">Home</a>
+          <a href="/">Services</a>
+          <a href="/">Portfolio</a>
+          <a href="/">Contact</a>
+          <a href="/">Mentions légales</a>
+        </nav>
+
+        <header className="">
           <h1>Bonjour, je suis John Doe</h1>
           <h2>Développeur Web full stack</h2>
+          <button>En savoir plus</button>
         </header>
-      </div>
+
+        <main>
+          <section>
+            <h3>A propos</h3>
+            <image></image>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce venenatis ante et neque venenatis malesuada. Sed viverra aliquam accumsan. Fusce quis imperdiet velit, nec consequat nisi. Pellentesque varius euismod mi, at eleifend turpis aliquam in. Aenean vitae consectetur elit. Ut lacus tortor, feugiat eget commodo eu, semper nec est. Nulla arcu diam, venenatis nec consequat a, aliquam at ex. Vivamus molestie lectus tellus, ut accumsan libero varius vitae.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce venenatis ante et neque venenatis malesuada. Sed viverra aliquam accumsan. Fusce quis imperdiet velit, nec consequat nisi. Pellentesque varius euismod mi, at eleifend turpis aliquam in. Aenean vitae consectetur elit. Ut lacus tortor, feugiat eget commodo eu, semper nec est. Nulla arcu diam, venenatis nec consequat a, aliquam at ex. Vivamus molestie lectus tellus, ut accumsan libero varius vitae.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce venenatis ante et neque venenatis malesuada. Sed viverra aliquam accumsan. Fusce quis imperdiet velit, nec consequat nisi. Pellentesque varius euismod mi, at eleifend turpis aliquam in. Aenean vitae consectetur elit. Ut lacus tortor, feugiat eget commodo eu, semper nec est. Nulla arcu diam, venenatis nec consequat a, aliquam at ex. Vivamus molestie lectus tellus, ut accumsan libero varius vitae.</p>
+          </section>
+          <section>
+            <h3>Mes compétences</h3>
+            <ul>
+              <li>HTML5 90%</li>
+
+              <li>CSS3 80%</li>
+
+              <li>Javascript 70%</li>
+
+              <li>PHP 60%</li>
+
+              <li>React 50%</li>
+
+            </ul>
+          </section>
+        </main>
+        
+        <footer>
+          <h4>John Doe</h4>
+          <address>
+            <p>40 rue Laure Diebold
+              69009 Lyon, France
+            </p>
+            <p>10 20 30 40 50
+            </p>
+            <p>johndoe@gmail.com
+            </p>
+          </address>
+            <a href="">
+            <i class="bi bi-github"></i>
+            </a>
+            <a href="">
+            <i class="bi bi-twitter"></i>
+            </a>
+            <a href="">
+            <i class="bi bi-linkedin"></i>
+            </a>
+          <h4>Liens utiles</h4>
+          <a href="/">Accueil</a>
+          <a href="/">Services</a>
+          <a href="/">Portfolio</a>
+          <a href="/">Contact</a>
+          <a href="/">Mentions légales</a>
+
+          <h4>Mes dernières réalisations</h4>
+          <a href="/">Fresh Food</a>
+          <a href="/">Restaurant Akira</a>
+          <a href="/">Espace bien-être</a>
+          <a href="/">SEO</a>
+          <a href="/">Création d'une API</a>
+          <a href="/">Maquette d'un site</a>
+
+        </footer>
+      </body>
     )
   }
 }
