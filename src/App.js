10d4a2834357070a48f1575e878829dd79f94406
Lucie Aimar <lucie.aimar13@gmail.com>
2025-05-14 20:51:14 +0200

    portfolio page html ok (+folder images moved to public folder)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Legalnotice from './pages/Legalnotice';
 
+
+
 class App extends React.Component {
   render() {
     return(
@@ -27,7 +29,49 @@ class App extends React.Component {
           <Route path="/legalnotice" element={<Legalnotice/>}></Route>
         </Routes>
 
-        
+        <footer>
+          <ul>
+            <li>
+              <h4>John Doe</h4>
+              <address>
+                <span>40 rue Laure Diebold</span>
+                <span>69009 Lyon, France</span>
+                <span>10 20 30 40 50</span>
+                <span>johndoe@gmail.com</span>
+              </address>
+              <a href="https://github.com/Johndoedoestai/john" target="blank">
+              <i class="bi bi-github"></i>
+              </a>
+              <a href="https://x.com/JohnDoe93668706" target="blank">
+              <i class="bi bi-twitter"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/john-doe-1236ab364/" target="blank">
+              <i class="bi bi-linkedin"></i>
+              </a>
+            </li>
+            <li>
+              <h4>Liens utiles</h4>
+              <ul>
+                <li><Link to="/">Accueil</Link></li>
+                <li><Link to="/services">Services</Link></li>
+                <li><Link to="/portfolio">Portfolio</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/legalnotice">Mentions légales</Link></li>
+              </ul>
+            </li>
+            <li>
+              <h4>Mes dernières réalisations</h4>
+              <ul>
+                <li><a href="/portfolio">Fresh Food</a></li>
+                <li><a href="/portfolio">Restaurant Akira</a></li>
+                <li><a href="/portfolio">Espace bien-être</a></li>
+                <li><a href="/portfolio">SEO</a></li>
+                <li><a href="/portfolio">Création d'une API</a></li>
+                <li><a href="/portfolio">Maquette d'un site</a></li>
+              </ul>
+            </li>
+          </ul>
+        </footer>
       </div>
     )
   }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,60 @@
 
+
 const Portfolio = () => {
     return (
         <div>
-            <h1>Portfolio</h1>
+            <header>
+                <img src="./images/banner.jpg" alt="une bannière bleue" />
+                <h1>Portfolio</h1>
+                <p>Voici quelques-unes de mes réalisations.</p>
+            </header>
+            <hr />
+            <main>
+                <section>
+                    <article>
+                        <img src="./images/portfolio/fresh-food.jpg" alt="une cagette avec des légumes" />
+                        <h2>Fresh Food</h2>
+                        <p>Site de vente de produits frais en ligne</p>
+                        <button>Voir le site</button>
+                        <aside>Site réalisé avec PHP et MySQL</aside>
+                    </article>
+                    <article>
+                        <img src="./images/portfolio/restaurant-japonais.jpg" alt="des sushis et des makis" />
+                        <h2>Restaurant Akira</h2>
+                        <p>Site de restaurant japonais</p>
+                        <button>Voir le site</button>
+                        <aside>Site réalisé avec WordPress</aside>
+                    </article>
+                    <article>
+                        <img src="./images/portfolio/espace-bien-etre.jpg" alt="une tête de bouddha, une fleur de lotus et une pile de galets avec un coucher de soleil en fond" />
+                        <h2>Espace bien-être</h2>
+                        <p>Site de vente de produits de bien-être</p>
+                        <button>Voir le site</button>
+                        <aside>Site réalisé avec LARAVEL</aside>
+                    </article>
+                    <article>
+                        <img src="./images/portfolio/seo.jpg" alt="des mots en rapport avec le seo sur fond bleu" />
+                        <h2>SEO</h2>
+                        <p>Amélioration du référencement d'un site e-commerce</p>
+                        <button>Voir le site</button>
+                        <aside>Utilisation des outils SEO</aside>
+                    </article>
+                    <article>
+                        <img src="./images/portfolio/coder.jpg" alt="du code informatique sur un écran pc" />
+                        <h2>Création d'une API</h2>
+                        <p>Création d'une API RESTFULL publique</p>
+                        <button>Voir le site</button>
+                        <aside>PHP - SYMFONY</aside>
+                    </article>
+                    <article>
+                        <img src="./images/portfolio/screens.jpg" alt="photo d'ordinateurs, un clavier et une tablette sur un bureau" />
+                        <h2>Maquette d'un site web</h2>
+                        <p>Création du prototype d'un site</p>
+                        <button>Voir le site</button>
+                        <aside>Réalisé avec FIGMA</aside>
+                    </article>
+                </section>
+            </main>
         </div>
     )
 }
