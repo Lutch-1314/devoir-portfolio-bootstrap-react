dacb897266eef25dec3f54060bdc3b915a242155
Lucie Aimar <lucie.aimar13@gmail.com>
2025-05-23 14:10:42 +0200

    dev Legalnotice html ok

--- src/pages/Legalnotice.jsx
+++ src/pages/Legalnotice.jsx
@@ -1,8 +1,61 @@
-
 const Legalnotice = () => {
     return (
         <div>
-            <h1>Mentions légales</h1>
+            <header>
+                <h1>Mentions légales</h1>
+            </header>
+            <main>
+                <section>
+                    <article>
+                        <h2>Éditeur du site</h2>
+                        <h3>John Doe</h3>
+                        <address>
+                            <span>
+                                <i class="bi bi-map"></i>
+                                40 rue Laure Diebold
+                            </span>
+                            <span>
+                                <i class="bi bi-geo-alt"></i>
+                                69009 Lyon, France
+                            </span>
+                            <span>
+                                <i class="bi bi-phone"></i>
+                                10 20 30 40 50
+                            </span>
+                            <span>
+                                <i class="bi bi-envelope-at"></i>
+                                johndoe@gmail.com
+                            </span>
+                        </address>
+                    </article>
+                    <article>
+                        <h2>Hébergeur</h2>
+                        <h3>alwaysdata</h3>
+                        <address>
+                            <span>
+                                91 Rue du Faubourg Saint-Honoré, 75008 Paris
+                            </span>
+                            <span>
+                                <i class="bi bi-globe"></i>
+                                <a href="https://www.alwaysdata.com/fr/" target="blank">www.alwaysdata.com</a>
+                            </span>
+                        </address>
+                    </article>
+                    <article>
+                        <h2>Crédits</h2>
+                        <h3>Crédits</h3>
+                        <p>Ce site a été réalisé par John Doe, étudiant au 
+                            <a href="https://www.centre-europeen-formation.fr/" target="blank"> Centre Européen de Formation.</a>
+                        </p>
+                        <p>Les images utilisées sur ce site sont libres de droits et ont été obtenues sur le site
+                            <a href="https://www.flaticon.com/fr/" target="blank"> Pixabay.</a>
+                        </p>
+                        <p>Le favicon de ce site a été fourni par 
+                            <a href="https://www.flaticon.com/de/kostenlose-icons/john-doe" target="blank"> John doe Icons erstellt von Freepik - Flaticon.</a>
+                        </p>
+                    </article>
+                </section>
+            </main>
         </div>
     )
 }
