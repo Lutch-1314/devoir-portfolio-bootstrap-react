8f89b8b405cc9f84ed7c9e41f4d3564e86654955
Lucie Aimar <lucie.aimar13@gmail.com>
2025-06-03 17:41:35 +0200

    in Navbar.jsx: indentation corrected + navbar-brand modified

--- src/other components/Navbar.jsx
+++ src/other components/Navbar.jsx
@@ -11,33 +11,33 @@ const Navbar = () => {
 
     return (
         <div>
-            <nav className="navbar navbar-dark navbar-expand-md bg-dark fixed-top text-uppercase px-4">
-          <div className="container-fluid">
-            <span className="navbar-brand text-uppercase">John Doe</span>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item">
-                  <NavLink className="nav-link active" aria-current="page" to="/" style={activeLink}>Accueil</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link active" aria-current="page" to="/services" style={activeLink}>Services</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link active" aria-current="page" to="/portfolio" style={activeLink}>Portfolio</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link active" aria-current="page" to="/contact" style={activeLink}>Contact</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link active" aria-current="page" to="/legalnotice" style={activeLink}>Mentions légales</NavLink>
-                </li>
-              </ul>
+          <nav className="navbar navbar-dark navbar-expand-md bg-dark fixed-top text-uppercase px-4">
+            <div className="container-fluid">
+              <span className="navbar-brand mb-0 h1">John Doe</span>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ms-auto">
+                  <li className="nav-item">
+                    <NavLink className="nav-link active" aria-current="page" to="/" style={activeLink}>Accueil</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link active" aria-current="page" to="/services" style={activeLink}>Services</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link active" aria-current="page" to="/portfolio" style={activeLink}>Portfolio</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link active" aria-current="page" to="/contact" style={activeLink}>Contact</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link active" aria-current="page" to="/legalnotice" style={activeLink}>Mentions légales</NavLink>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </nav>
+          </nav>
         </div>
     )
 }
